f49da280c5e9b2a31b3e2707d8124f34d86a372c
Laura Phoebus <lau12100@esri.com>
2025-06-25 14:47:14 -0400

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/migration-app/',
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js', // removes leading underscore
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
 });
